691a05d8cf28c0e7a70681a0a4ab6573dbb5536c
JakubKrajes <92035401+JakubKrajes@users.noreply.github.com>
2022-11-10 11:59:13 +0100

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,77 +1,112 @@
 
 from typing import Any
 
+
 class Node:
     value: Any
     next: 'Node'
-    def __init__(self, value: Any)->None:
-        self.value=value
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+
 
 class LinkedList:
     head: Node
     tail: Node
-    def __init__(self)->None:
+
+    def __init__(self):
         self.head = None
         self.tail = None
-        
-    def push(self,element: Any)->None:
-        node = Node(element)
+
+    def push(self, value: Any):
+        new_node = Node(value)
         if self.head is None:
-            self.head = node
-            self.tail = node
+            self.head = new_node  # if list is empty, head and tail point to the same node (same value)
+            self.tail = new_node
         else:
-            node.next = self.tail
-            self.head = node
+            new_node.next = self.head  # moves head to another node
+            self.head = new_node  # new node has become the head
 
-    def append(self,element):
-        lastnode = Node(element)
-        lastnode.next = None
-        self.tail = lastnode
+    def append(self, value: Any) -> None:
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node  # if list is empty, head and tail point to the same node (same value)
+            self.tail = new_node
+        else:
+            self.tail.next = new_node  # moves tail to another node
+            self.tail = new_node  # new node has become the tail
 
-    def len(self):
-        node=self.head
-        len=0
+    def node(self, at: int) -> Node:
+        index = 0
+        if at == 0:
+            return self.head
+        if self.head is None:
+            return None
+        temp = self.head  # temp is at head
+        while temp:
+            if index == at:  # if we're looking for head (index 0), return head
+                return temp
+            index += 1
+            temp = temp.next
+        return temp
 
-        while node is not None:
-            len=len+1
-            node=node.next
-        return len
+    def insert(self, value: Any, after: Node) -> None:
+        if self.head is None:
+            self.head = value  # if list is empty, it gives head the value
+        new_node = Node(value)
+        new_node.next = after.next  # connects new node with next node after the after
+        after.next = new_node  # adds new node after the after
 
-    def remove_last(self):
-        element = self.tai
-         i=0
+    def pop(self) -> Any:
+        if self.head is None:
+            self.tail = None
+            return None
+        temp = self.head
+        self.head = self.head.next  # moves head to next node
+        temp.next = None  # disconects temp from list
+        if self.head.next is None:
+            self.tail = None
+        return temp.value
 
-        while(i<after):
-            currentNode=currentNode.next
-            i=i+1
+    def remove_last(self) -> Any:
+        if self.head is None:
+            return None
+        temp = self.head
+        temp_value = self.head
+        while (temp.next):  # goes through next nodes
+            temp_value = temp  # saves the last searched node value
+            temp = temp.next  # moves to next node
+        self.tail = temp_value  # saves new tail
+        self.tail.next = None  # removes (pops) previous tail
+        return temp.value
 
-        currentNode.next=currentNode.next.next
-  
-    def print(self)->None:
-        node=self.head
+    def remove(self, after: Node) -> Any:
+        temp = after.next  # temp to remove the node
+        after.next = temp.next  # connects previous node with next node after the after node
+        temp.next = None  # temp node gets removed
 
-        while node is not None:
-            print(node.value,"-> ", end="")
-            node=node.next
-        print("None")
-list_=LinkedList()
-list_.append(3)
-list_.append(4)
-list_.push(5)
-#print(list_.NodePos(0))
-list_.print()
-list_.insertNode(8,1)
-list_.print()
-print(list_.pop())
-print(list_.removeLast())
-list_.print()
-list_.append(6)
-list_.append(10)
-list_.print()
-list_.remove(1)
-list_.print()
-print(list_.len())
-#LIFO
+    def __str__(self):
+        temp = self.head
+        return_str = ""  # if head is empty, returns empty
+        if temp is not None:
+            while temp is not None:
+                return_str += str(temp.value)  # adds value of temp node
+                temp = temp.next  # moves to next node
+                if temp is not None:
+                    return_str += " -> "  # adds arrows between
+        return return_str
+
+    def __len__(self):
+        temp = self.head
+        count = 0
+        while temp:
+            count += 1
+            temp = temp.next
+        return count
 
 class Node:
     def __init__(self, value=None, next=None):
