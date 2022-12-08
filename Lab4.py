ceb6789983a1190357cc6ca5e415f40213dcb75c
JakubKrajes <92035401+JakubKrajes@users.noreply.github.com>
2022-12-08 11:39:55 +0100

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -1,19 +1,44 @@
-from typing import Any
-import graphviz
+from typing import Any, List
 
 
-
-class TreeNode:
+class BinaryNode:
     value: Any
-    children: List["TreeNode"]
+    left_child: 'BinaryNode'
+    right_child: 'BinaryNode'
 
     def __init__(self, value):
         self.value = value
-        self.children = []
+        self.left_child = None
+        self.right_child = None
 
-    def is_leaf(self) -> bool:
-        if self.children is None:
-            return True
+    def __str__(self) -> str:
+        return str(self.value)
 
-    def add(self, child: 'TreeNode') -> None:
-        self.children.append(child)
+    def min(self) -> 'BinaryNode':
+        if self.left_child > self.right_child:
+            print("To nie jest BST")
+        elif self.left_child is not None:
+            return self.left_child.min()
+        return self
+
+
+class BinarySearchTree:
+    root: BinaryNode
+
+    def __init__(self, value):
+        self.root = BinaryNode(value)
+
+    def insert(self, value: Any) -> None:
+        self.root = self._insert(self.root, value)
+
+    def _insert(self, node: BinaryNode, value: Any) -> BinaryNode:
+        if node is None:
+            return BinaryNode(value)
+
+        if value < node.value:
+            node.left_child = self._insert(node.left_child, value)
+
+        if value >= node.value:
+            node.right_child = self._insert(node.right_child, value)
+
+        return node
